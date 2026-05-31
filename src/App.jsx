00c2bd15f8c73eaeb32277148e0cf9950a5b1930
E-Was24 <satriaelan5@gmail.com>
2026-05-31 09:32:31 +0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/LandingPage'; 
 import ElanPage from './pages/ElanPage';
 import RiskaPage from './pages/RiskaPage';
+import './App.css'
 
 function App() {
   return (
