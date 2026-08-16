0a5d48ceb107fc8fe71d4ab7b90526321dbd38e3
e-was24 <satriaelan5@gmail.com>
2026-08-17 05:16:05 +0700

    update journey page

--- src/pages/Journey.jsx
+++ src/pages/Journey.jsx
@@ -3,12 +3,9 @@ import gsap from "gsap";
 import React, { useRef, useState } from "react";
 import TextPlugin from "gsap/TextPlugin";
 import { Link } from "react-router-dom";
-import loadingSvg from '../components/journeyLoading.svg';
-import catLoader from '../components/catLoader.gif'
-
+import catLoader from '../components/catLoader.gif';
 
 import "./css/journey-style.css";
-import { div } from "framer-motion/client";
 
 gsap.registerPlugin(TextPlugin);
 
@@ -21,14 +18,15 @@ export default function Journey() {
     const loadingTl = gsap.timeline();
 
     loadingTl.to(".journey-loading-container", {
-      duration: 9,
+      duration: 2.5, // Dibuat sedikit lebih pas untuk durasi loading
       opacity: 0,
       onComplete: () => {
-        (setIsLoading(false),
-          gsap.to(".layout", {
-            duration: 5,
-            display: "block",
-          }));
+        setIsLoading(false);
+        gsap.to(".layout", {
+          duration: 1.5,
+          display: "block",
+          opacity: 1,
+        });
       },
     });
 
@@ -40,12 +38,16 @@ export default function Journey() {
 
   return (
     <div ref={container}>
-      {isLoading && <div className="journey-loading-container"><img src={catLoader} alt="Loading" /></div>}
+      {isLoading && (
+        <div className="journey-loading-container">
+          <img src={catLoader} alt="Loading" />
+        </div>
+      )}
       <div className="layout" ref={layoutRef}>
         <div className="drop-line">
           <h2 className="title-years">2026</h2>
           <div className="line-v">
-            {/* sesi 1 awkwka */}
+            {/* Sesi Bulan */}
             <h1 className="title-month">AGU</h1>
             <div className="line-h-1"></div>
             <Link to="/galery/solo" className="link">
