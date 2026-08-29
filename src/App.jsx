c905f247754ad7fb8cbd5c3419030afb0ea03a26
Elan Mas <168735717+e-was24@users.noreply.github.com>
2026-08-30 00:38:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Journey from "./pages/Journey";
 import Galery from "./pages/Galery";
 import AccessGate from "./components/AccessGate";
 import ExpiredPage from "./pages/Expired";
-import Mei from "./next-gen/mei";
 import "./App.css";
 
 function App() {
@@ -31,7 +30,6 @@ function App() {
               <Route path="shop" element={<Shop />} />
               <Route path="journey" element={<Journey />} />
               <Route path="token-expired" element={<ExpiredPage />} />
-              <Route path="mei" element={<Mei />} />
             </Route>
           </Route>
         </Routes>
